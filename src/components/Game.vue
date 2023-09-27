68144f2b2ff53203088ed29cdc525e8fba1e95c5
TheExpandingPolytope <jesseengerman@hotmail.com>
2023-09-27 14:36:11 -0500

    pointer cursor

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -30,7 +30,7 @@
           <div style="display:flex; align-items:center" v-if="fetchingEvents"><p>Syncing events</p><div class="small-loading"></div></div>
 
     <div v-if="!isInGame && !isWaiting">
-        <p style="margin:0; margin-bottom:6px;">Your balance &nbsp; <span  @click="this.$store.dispatch('clearAndReload')">&#x21bb;</span></p>
+        <p style="margin:0; margin-bottom:6px;">Your balance &nbsp; <span style="cursor:pointer;"  @click="this.$store.dispatch('clearAndReload')">&#x21bb;</span></p>
       <h1 style="margin:0; margin-bottom:22px;">
         
         {{this.balance?.toString().split(".")[0]}}
